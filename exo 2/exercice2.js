df6203191a5b67291f6862649e4c56fc91e47dc9
victorien <deadvicmangame@gmail.com>
2022-12-06 10:44:38 +0100

    exo 83 Tableaux js

--- exo 2/exercice2.js
+++ exo 2/exercice2.js
@@ -1,7 +1,7 @@
 let prenoms = ["pierre","paul","jacques"];
-    = "<Ton Prénom>";
+prenoms[0] = "Votre Prénom";
 
-document.getElementById('viewport'). = ;
+document.getElementById('viewport').innerText = prenoms[0];
 
 
 
